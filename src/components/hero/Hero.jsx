5f3acb04d2d69952b1256bc990d97d8d6eea3188
OptimusJc <optimus.prime.jc@gmail.com>
2022-08-31 19:15:26 +0300

    style: Add the hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,37 @@
+import "./Hero.modules.css";
+
+import my_image from "../../assets/my_image_blob.png";
+
+const Hero = () => {
+	return (
+		<section className="hero">
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<div className="intro__section">
+							<p>Hello I'm,</p>
+							<h1>
+								Joseph <span>Otieno</span>
+							</h1>
+							<p>
+								Enthusiastic developer eager to contribute to team success
+								through hard work, attention to detail and excellent
+								organizational skills. Clear understanding of JavaScript, CSS,
+								HTML, React and currently training on Python. Motivated to
+								learn, grow and excel in the entire fullstack development
+								module.
+							</p>
+							<p className="title">Fullstack Web Developer</p>
+							<a href="#">download cv</a>
+						</div>
+						<div className="image__section">
+							<img src={my_image} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
