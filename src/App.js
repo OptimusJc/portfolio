4a7de2c04539378f25bcc84425d0c7e8b441895e
OptimusJc <optimus.prime.jc@gmail.com>
2022-09-02 12:50:53 +0300

    style: Add other projects to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./index.css";
 
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import OtherProjects from "./components/projects/otherProjects/OtherProjects";
 
 function App() {
 	return (
@@ -9,6 +13,10 @@ function App() {
 			<section className="hero__background">
 				<Header />
 				<Hero />
+				<About />
+				<Skills />
+				<Projects />
+				<OtherProjects />
 			</section>
 		</div>
 	);
